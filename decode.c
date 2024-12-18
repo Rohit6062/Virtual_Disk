dba2be17508bf3b04fb28a931e20bf8025a405c0
Rohit6062 <rohitranaware0096@gmail.com>
2024-12-18 11:21:15 +0530

    readability

--- decode.c
+++ decode.c
@@ -1,7 +1,7 @@
 #include"vdisk_header.h"
 long decode(diskinfo* vdisk){
     byte read = getc(vdisk->f);
-    if(vdisk->currBit==0)read = fgetc(vdisk->f),vdisk->currBit=8;
+    if(!vdisk->currBit)read = fgetc(vdisk->f),vdisk->currBit=8;
     byte lvl;
     long output=0;
     long toget = 2;
@@ -10,25 +10,26 @@ long decode(diskinfo* vdisk){
         else lvl =  (1 & read) << 2 , read = getc(vdisk->f) , lvl = lvl | (3 & (read >> 6)), vdisk->currBit = 6;
     } 
     else lvl = 7 & (read >> (vdisk->currBit - 3)), vdisk->currBit= vdisk->currBit-3;
-    if(vdisk->currBit==0)vdisk->currBit=8,read = getc(vdisk->f);
-    while(lvl--){
-        output = 0;
-        while(toget){
-            if(toget<vdisk->currBit){
-                output = output | (makeToget(toget) & (read >> (vdisk->currBit-toget)));  
-                vdisk->currBit = vdisk->currBit-toget;
-                toget = 0;
-            }
-            else{
-                output = output |  (makeToget(vdisk->currBit) & read );
-                toget -= vdisk->currBit;
-                if(toget>8)output = output << 8;
-                else output = output << toget;
-                vdisk->currBit = 8;
-                read = getc(vdisk->f);
-            }
+    if(!vdisk->currBit)vdisk->currBit=8,read = getc(vdisk->f);
+    while(lvl){
+        if(!toget){
+            toget = output;
+            lvl--;
+            if(lvl)output = 0;
         }
-        toget = output;
+        else if(toget<vdisk->currBit){
+            output = output | (makeToget(toget) & (read >> (vdisk->currBit-toget)));  
+            vdisk->currBit = vdisk->currBit-toget;
+            toget = 0;
+        }
+        else{
+            output = output |  (makeToget(vdisk->currBit) & read );
+            toget -= vdisk->currBit;
+            if(toget>8)output = output << 8;
+            else output = output << toget;
+            vdisk->currBit = 8;
+            read = getc(vdisk->f);
+        }
     }
     fseek(vdisk->f,-1,SEEK_CUR);
     return output;
